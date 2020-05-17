1a29f87a72c3abb369ec6163d27f01af5d8b370f
Ruslan Drozdovskyi <ruslan.drozdovskyi@goodahead.com>
2020-05-17 18:04:58 +0300

    Lesson #43

--- app/code/MyMagento/Todo/Api/Data/TaskInterface.php
+++ app/code/MyMagento/Todo/Api/Data/TaskInterface.php
@@ -3,10 +3,25 @@ declare(strict_types=1);
 
 namespace MyMagento\Todo\Api\Data;
 
+use Magento\Tests\NamingConvention\true\string;
+
 /**
  * @api
  */
 interface TaskInterface
 {
+    /**
+     * @return int
+     */
+    public function getTaskId(): int;
 
+    /**'
+     * @return string
+     */
+    public function getStatus(): string;
+
+    /**
+     * @return string
+     */
+    public function getLabel(): string;
 }

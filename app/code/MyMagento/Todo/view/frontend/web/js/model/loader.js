46179998c5a76cb239da6806bed41ca61ffc0c96
Ruslan Drozdovskyi <ruslan.drozdovskyi@goodahead.com>
2020-05-26 00:43:46 +0300

    Lesson #56

---
+++ app/code/MyMagento/Todo/view/frontend/web/js/model/loader.js
@@ -0,0 +1,19 @@
+define([
+   'jquery',
+   'rjsResolver'
+], function ($, resolver) {
+    'use strict';
+    const containerId = '.tasks';
+
+    return {
+        startLoader: function () {
+            $(containerId).trigger('processStart');
+        },
+
+        stopLoader: function (forceStop) {
+            var $elem = $(containerId),
+                stop = $elem.trigger.bind($elem, 'processStop');
+            forceStop ? stop() : resolver(stop);
+        }
+    };
+});
